d6ab2a23c8465fc63eabda5ec0de66005f1963ee
Yoni BenTov <48489482+yonibentov@users.noreply.github.com>
2026-03-11 14:14:06 -0400

    Updated search.h and search.c

---
+++ source/cargo/search.h
@@ -0,0 +1,28 @@
+/* Interface for search algorithms for the cargo-loading problem */
+
+#ifndef CARGO_LOADING_SEARCH_H
+#define CARGO_LOADING_SEARCH_H
+
+/* Search over allowable allocations at fixed capacity and stage.
+   
+   Inputs:
+     * An array of previous returns;
+     * A value (v_n, of type Int) and a weight (w_n, of type Int);
+     * The current capacity (c, of type Int) being considered.
+     
+   Outputs:
+     * bestPolicy (of type Int);
+     * bestReturn (of type Int). */
+
+#include "common.h"
+#include "array.h"
+
+void cargoLoading_search(Int* bestPolicy,  // output (read-write)
+			 Int* bestReturn,  // output (read-write)
+			 ArrayInt* previousReturns,  // input (read only)
+			 Int value,  // input (read only)
+			 Int weight,  // input (read only)
+			 Int capacity  // input (read only)
+			 );
+
+#endif /* CARGO_LOADING_SEARCH_H */
